20308617ed6f2734e40524701c29ad366640c9f1
Bence Tamas <mr.bence.tamas@gmail.com>
2014-02-12 17:29:21 +0100

    fix env vars and startup script

--- job_task.py
+++ job_task.py
@@ -22,14 +22,16 @@ from pimployee.switchboard_client import UnixDomainSocketClient
 try:
     qdesc = os.environ["QDESC"]
     wid = os.environ["WID"]
-except KeyError:
-    raise Exception('environment variable QDESC and/or WID are not set')
+    address, port = os.environ["GATEWAY"].split(":")
+    port = int(port)
+except Exception:
+    raise Exception('environment variables QDESC/ID/GATEWAY are not set or invalid')
 
 setup_util.socket_set_default_timeout(60.0)
 
 # creates a connection to the boss using a unix domain socket
 # that the boss has made and placed on the filesystem
-s = setup_util.connect_to_boss()
+s = setup_util.connect_to_boss(address, port)
 
 # create client object for easy sending and receiving of messages
 c = UnixDomainSocketClient(s)
@@ -57,7 +59,7 @@ setup_util.setup_sys_path(m.meta.get('ap_version'), m.meta.get('ap_path'), m.met
 setup_util.setup_cloud(m.meta['hostname'], m.meta['ap_version'])
 
 # save stderr descriptor, and restore on exit for atexit
-orig_stderr_fd = os.dup(sys.stderr.fileno())    
+orig_stderr_fd = os.dup(sys.stderr.fileno())
 
 # keys map to "type" of Message.meta['type']
 message_handlers = {'stdout': setup_util.substitute_stdout_pipe,
@@ -69,9 +71,9 @@ message_handlers = {'stdout': setup_util.substitute_stdout_pipe,
                     'die': lambda m: None}
 
 while True:
-    
+
     log.logger.info('Waiting for message from boss')
-    
+
     m = c.read()
     handler = message_handlers.get(m.meta['type'])
     if handler:
@@ -80,7 +82,7 @@ while True:
         log.logger.info('Done handling message %s m.meta' % m.meta)
     else:
         log.logger.info('Unrecognized message type %s' % m.meta['type'])
-    
+
     if m.meta['type'] == 'assign':
         try:
             job_util.process_job(m, c)
@@ -89,14 +91,14 @@ while True:
         except job_util.EndProcessException:
             log.logger.info('EndProcessException raised. Process will end')
             break
-    
+
     if m.meta['type'] == 'die':
         log.logger.info('Received message to die.')
-        
+
         # restore stderr for atexit to work
-        sys.stderr = sys.__stderr__             
+        sys.stderr = sys.__stderr__
         os.dup2(orig_stderr_fd, sys.stderr.fileno())
-        
+
         break
 c.kill()
 # an interpreter shutdown sometimes crashes. we don't want faulthandler
@@ -108,4 +110,3 @@ log.logger.info('last statement in job_task before sys.exit(0)')
 # force exit, otherwise non-daemon threads could keep the process alive
 # indefinitely
 sys.exit(0)
-
